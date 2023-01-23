6a7b64282a254edfa517faf583fd771ab279f653
laj <lajsotec@gmail.com>
2023-01-23 17:34:02 +0100

    v1.0.2

--- Quimera/src/main/java/com/weewoo/quimera/LajButton.kt
+++ Quimera/src/main/java/com/weewoo/quimera/LajButton.kt
@@ -17,6 +17,6 @@ class LajButton @JvmOverloads constructor(
     init{
         LayoutInflater.from(context).inflate(R.layout.component_btn,this, true)
         var btn = findViewById<Button>(R.id.btnQuimera)
-        btn.setOnClickListener{Toast.makeText(context, "Esta es la version 1.1.3, hola", Toast.LENGTH_LONG).show()}
+        btn.setOnClickListener{Toast.makeText(context, "Esta es la version 1.0.2", Toast.LENGTH_LONG).show()}
     }
 }
